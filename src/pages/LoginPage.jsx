9e3342cf3d1339926e6d8ef314ab0de91f8af2b0
NahuelSantos98 <santosnahuel98@gmail.com>
2024-05-31 02:17:45 -0300

    Estilado login hecho

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import FormLogin from '../Components/Auth/FormLogin'
+import React from 'react';
+import FormLogin from '../Components/Auth/FormLogin';
+import style from '../styles/authStyles/loginPage.module.css';
+import movieChairsImage from '../assets/img/movie-chairs.jpeg'
 
 const LoginPage = () => {
   return (
-    <>
-        <h1>Welcome to our Movie Blog</h1>
-        <h3 style={{textAlign: 'center', margin: '2rem'}}>To continue you have to Log in or if you do not have an account register</h3>
-        <FormLogin />
-    </>
-  )
-}
+    <div>
+      <h1 className={style.title}>Welcome to our Movie Blog</h1>
+      <h3 className={style.subtitle}>To continue you have to Log in or if you do not have an account register</h3>
+      <FormLogin />
+      <div className={style.imageContainer}>
+        <img className={style.chairs} src={movieChairsImage} alt="Movie Chairs" />
+      </div>
+    </div>
+  );
+};
 
-export default LoginPage
+export default LoginPage;
